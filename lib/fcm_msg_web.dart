fb63418989744cfe5ff28ac2339662eddb985750
Bikramaditya <bikram.magnigeeks@gmail.com>
2024-08-13 10:53:30 +0530

    - new fcm msg web added

--- lib/fcm_msg_web.dart
+++ lib/fcm_msg_web.dart
@@ -24,43 +24,21 @@ import 'src/utils.dart' as utils;
 
 /// Web implementation for [FirebaseMessagingPlatform]
 /// delegates calls to messaging web plugin.
-class FirebaseMessagingWeb extends FirebaseMessagingPlatform {
+class FcmMsgWeb extends FirebaseMessagingPlatform {
   String? vapidKey;
-  /// Instance of Messaging from the web plugin
-  messaging_interop.Messaging? _webMessaging;
-
-  Future<messaging_interop.Messaging?> get _delegate async {
-    try {
-      _webMessaging ??=
-          messaging_interop.getMessagingInstance(core_interop.app(app.name));
-    }catch(e){
-      print("error on init fcm firebase");
-    }
-    if (!_initialized) {
-      // _webMessaging!.onMessage
-      //     .listen((messaging_interop.MessagePayload webMessagePayload) {
-      //   RemoteMessage remoteMessage =
-      //       RemoteMessage.fromMap(utils.messagePayloadToMap(webMessagePayload));
-      //   FirebaseMessagingPlatform.onMessage.add(remoteMessage);
-      // });
-      await initFBMsg(config: app.options).then((value) => onMessage());
-      _initialized = true;
-    }
-
-    return _webMessaging;
-  }
 
   /// Called by PluginRegistry to register this plugin for Flutter Web
   static Future<void> registerWith(Registrar registrar) async {
     await importJsLibrary(
-      url: "firebase-config.js",);
+      url: "firebase-config.js",
+    );
     await importJsLibrary(
-    url: "./assets/firebase_msg.js", flutterPluginName: "firebase_messaging_web");
-    FirebaseCoreWeb.registerService('messaging');
-    FirebaseMessagingPlatform.instance = FirebaseMessagingWeb();
+        url: "./assets/firebase_msg.js",
+        flutterPluginName: "firebase_messaging_web");
   }
+
   Future<void> initFBMsg({required FirebaseOptions config}) async {
-     await promiseToFuture(js.initFBMsg(jsify(config.asMap)));
+    await promiseToFuture(js.initFBMsg(jsify(config.asMap)));
   }
 
   void onMessage({Function(RemoteMessage)? onMessage}) {
@@ -75,34 +53,15 @@ class FirebaseMessagingWeb extends FirebaseMessagingPlatform {
           ),
           data: Map<String, dynamic>.from(data),
         );
-        if(onMessage!=null){
+        if (onMessage != null) {
           onMessage.call(remoteMessage);
         }
         FirebaseMessagingPlatform.onMessage.add(remoteMessage);
       }
     });
   }
+
   Stream<String>? _noopOnTokenRefreshStream;
-
-  static bool _initialized = false;
-
-  /// Builds an instance of [FirebaseMessagingWeb] with an optional [FirebaseApp] instance
-  /// If [app] is null then the created instance will use the default [FirebaseApp]
-  FirebaseMessagingWeb({FirebaseApp? app}) : super(appInstance: app);
-
-  /// Updates user on browser support for Firebase.Messaging
-  @override
-  Future<bool> isSupported() {
-    return messaging_interop.Messaging.isSupported();
-  }
-
-  @override
-  void registerBackgroundMessageHandler(BackgroundMessageHandler handler) {}
-
-  @override
-  FirebaseMessagingPlatform delegateFor({required FirebaseApp app}) {
-    return FirebaseMessagingWeb(app: app);
-  }
 
   @override
   FirebaseMessagingPlatform setInitialValues({bool? isAutoInitEnabled}) {
@@ -124,12 +83,6 @@ class FirebaseMessagingWeb extends FirebaseMessagingPlatform {
 
   @override
   Future<void> deleteToken() async {
-    await _delegate;
-
-    if (!_initialized) {
-      // no-op for unsupported browsers
-      return;
-    }
     await promiseToFuture(js.deleteToken());
 
     // return convertWebExceptions(_delegate.deleteToken);
@@ -142,13 +95,6 @@ class FirebaseMessagingWeb extends FirebaseMessagingPlatform {
 
   @override
   Future<String?> getToken({String? vapidKey}) async {
-    await _delegate;
-
-    if (!_initialized) {
-      // no-op for unsupported browsers
-      return null;
-    }
-
     this.vapidKey = vapidKey;
     // String t =  await promiseToFuture(js.getToken(vapidKey));
     // return t;
